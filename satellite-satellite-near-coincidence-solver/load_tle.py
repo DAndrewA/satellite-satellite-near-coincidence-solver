5cabeeadd35d50a6a7447cfcec8322f9068b1b05
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-05-15 15:06:14 +0100

    load_tle changed to correctly use timescale

--- satellite-satellite-near-coincidence-solver/load_tle.py
+++ satellite-satellite-near-coincidence-solver/load_tle.py
@@ -1,32 +1,31 @@
 """Module allowing the loading of TLE files or OMM files to create mutliple skyfield.sgp4lib.EarthSatellite instances"""
 
 import skyfield.api import EarthSatellite
+from skyfield.timelib import Timescale
 from typing import Any
 import os
 import datetime as dt
 
 type OMM_DICT = dict[str, Any]
 
-def from_OMM_dict(omm_dict: dict[Any, OMM_DICT]) -> dict[Any, EarthSatellite]:
-    """Function to transform dictionary of OMM dictionaries into a dictionary of EarthSatellite instances with the same associated keys"""
-    return {
-        key: EarthSatellite.from_omm(omm)
-        for key, omm in omm_dict.items()
-    }
 
-
-def from_OMM_JSON(fpath: str):
+def read_omm_json(fpath: str) -> OMM_DICT | list[OMM_DICT]:
     assert os.path.isfile(fpath), f"{fpath=} is not a valid file"
     with open(fpath, "r") as f:
-        omm_json = json.loads(f.read())
+        omm_json = json.load(f)
+    return omm_json
+
+
+def from_json(fpath: str, ts: Timescale):
+    omm_json = read_omm_json(fpath)
 
     if isinstance(omm_json, dict):
-        return EarthSatellite.from_omm(omm_json)
+        return EarthSatellite.from_omm(ts=ts, element_dict=omm_json)
     elif isinstance(omm_json, list):
         return from_OMM_dict(
             omm_dict = {
-                dt.datetime.fromisoformat(v["EPOCH"]): v
-                for v in omm_json
+                dt.datetime.fromisoformat(omm["EPOCH"]): EarthSatellite.from_omm(ts=ts, element_dict=omm)
+                for omm in omm_json
             }
         )
     else:
